ea48299ff68c1e44994524456326fb5fd3888045
Abdulkeza <adeoabdul.n@gmail.com>
2024-01-24 11:18:58 +0200

    add function which respond generated pdf

--- core/src/main/java/it/codeland/forms/core/servlets/PdfGenerationServlet.java
+++ core/src/main/java/it/codeland/forms/core/servlets/PdfGenerationServlet.java
@@ -1,18 +1,21 @@
 package it.codeland.forms.core.servlets;
 
 import javax.servlet.Servlet;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import java.io.File;
-import java.io.IOException;
-
 
 import com.adobe.aemfd.docmanager.Document;
 import com.adobe.fd.forms.api.FormsService;
-import javax.servlet.http.HttpServletResponse;
 
 @Component(service = { Servlet.class }, property = { "sling.servlet.methods=post",
         "sling.servlet.paths=/bin/generatePdf" })
@@ -37,18 +40,40 @@ public class PdfGenerationServlet extends SlingAllMethodsServlet {
             xml_is = xml_data_part.getInputStream();
             Document data_merged_document = formsService.importData(new Document(pdf_document_is),
                     new Document(xml_is));
+
+            // Save the document to the specified file path
             data_merged_document.copyToFile(new File(file_path));
 
-            //response in my postman
+            // Convert PDF content to a byte array
+            byte[] pdfByteArray = convertInputStreamToByteArray(data_merged_document.getInputStream());
 
+            // Set the content type and headers for PDF response
+            response.setContentType("application/pdf");
+            response.setHeader("Content-Disposition", "inline; filename=\"" + file_path + "\"");
+
+            // Write the PDF content to the response
+            try (ServletOutputStream out = response.getOutputStream()) {
+                out.write(pdfByteArray);
+            }
 
         } catch (Exception e) {
             try {
-                response.sendError(400, e.getMessage());
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
             } catch (IOException e1) {
-                // TODO Auto-generated catch block
                 e1.printStackTrace();
             }
         }
+    }
+
+    private byte[] convertInputStreamToByteArray(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byteArrayOutputStream.write(buffer, 0, bytesRead);
+        }
+
+        return byteArrayOutputStream.toByteArray();
     }
 }
