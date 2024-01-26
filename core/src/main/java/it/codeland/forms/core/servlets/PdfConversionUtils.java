ee5ca1baabba7ec18b82dbb3f1f0cb5e2a5d291d
Abdulkeza <adeoabdul.n@gmail.com>
2024-01-26 11:47:03 +0200

    add pdf conversion utils

---
+++ core/src/main/java/it/codeland/forms/core/servlets/PdfConversionUtils.java
@@ -0,0 +1,48 @@
+package it.codeland.forms.core.servlets;
+
+import com.adobe.aemfd.docmanager.Document;
+import com.adobe.fd.forms.api.FormsService;
+import com.adobe.fd.forms.api.PDFFormRenderOptions;
+
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+
+public class PdfConversionUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(PdfConversionUtils.class);
+
+    @Reference
+    FormsService formsService;
+   // private FormsService formsService; // Inject this dependency through @Reference or instantiate it appropriately
+
+    /**
+     * Converts an XDP InputStream to a dynamic PDF InputStream.
+     *
+     * @param xdpInputStream InputStream of the XDP file
+     * @return InputStream representing the generated dynamic PDF; null in case of an error
+     */
+    public InputStream convertXdpToDynamicPdf(InputStream xdpInputStream) {
+        try {
+
+            log.info("++++++converting xdp started++++++");
+            // Specify render options if needed
+            PDFFormRenderOptions renderOptions = new PDFFormRenderOptions();
+            renderOptions.setAcrobatVersion(com.adobe.fd.forms.api.AcrobatVersion.Acrobat_11);
+
+            // Perform XDP to dynamic PDF conversion
+            Document dynamicPdfDocument = formsService.renderPDFForm("/content/dam/path/to/your/template.xdp", null, renderOptions);
+
+            // Convert the dynamic PDF Document to InputStream
+            return dynamicPdfDocument.getInputStream();
+        } catch (Exception e) {
+            // Log the error using SLF4J
+            log.error("+++++++++XDP to dynamic PDF conversion failed: {}", e.getMessage(), e);
+            return null;
+        }
+    }
+
+
+}
--- core/src/main/java/it/codeland/forms/core/servlets/SavePdf.java
+++ core/src/main/java/it/codeland/forms/core/servlets/SavePdf.java
@@ -13,24 +13,31 @@ import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.adobe.aemfd.docmanager.Document;
 import com.adobe.fd.forms.api.FormsService;
 
 @Component(service = { Servlet.class }, property = { "sling.servlet.methods=post",
-        "sling.servlet.paths=/bin/generatePdf" })
-public class PdfGenerationServlet extends SlingAllMethodsServlet {
+        "sling.servlet.paths=/bin/save_me" })
+public class SavePdf extends SlingAllMethodsServlet {
 
     private static final long serialVersionUID = 1L;
 
     @Reference
     FormsService formsService;
 
+    @Reference
+    PdfConversionUtils pdfConversionUtils; // Inject or instantiate appropriately
+
+    private static final Logger log = LoggerFactory.getLogger(SavePdf.class);
+
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) {
         String file_path = request.getParameter("save_location");
-
-        java.io.InputStream pdf_document_is = null;
-        java.io.InputStream xml_is = null;
+    
+        InputStream pdf_document_is = null;
+        InputStream xml_is = null;
         javax.servlet.http.Part pdf_document_part = null;
         javax.servlet.http.Part xml_data_part = null;
         try {
@@ -38,33 +45,41 @@ public class PdfGenerationServlet extends SlingAllMethodsServlet {
             xml_data_part = request.getPart("xml_data_file");
             pdf_document_is = pdf_document_part.getInputStream();
             xml_is = xml_data_part.getInputStream();
+    
+            // Check if the uploaded file is an XDP file
+            if (isXdpFile(pdf_document_part)) {
+                log.info("############Checking xdp file has started######");
+                // Convert XDP to dynamic PDF
+                pdf_document_is = pdfConversionUtils.convertXdpToDynamicPdf(pdf_document_is);
+            }
+    
             Document data_merged_document = formsService.importData(new Document(pdf_document_is),
                     new Document(xml_is));
-
-   
+    
             data_merged_document.copyToFile(new File(file_path));
-
-
+    
             byte[] pdfByteArray = convertInputStreamToByteArray(data_merged_document.getInputStream());
-
-   
+    
             response.setContentType("application/pdf");
             response.setHeader("Content-Disposition", "inline; filename=\"" + file_path + "\"");
-
-            //  PDF content to the response
+    
+            // PDF content to the response
             try (ServletOutputStream out = response.getOutputStream()) {
                 out.write(pdfByteArray);
             }
-
+    
         } catch (Exception e) {
+            // Log the error using SLF4J
+            log.error("Error in SavePdf servlet: {}", e.getMessage(), e);
+    
             try {
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
             } catch (IOException e1) {
-                e1.printStackTrace();
+                log.error("Error sending error response: {}", e1.getMessage(), e1);
             }
         }
     }
-
+    
     private byte[] convertInputStreamToByteArray(InputStream inputStream) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         byte[] buffer = new byte[1024];
@@ -77,9 +92,8 @@ public class PdfGenerationServlet extends SlingAllMethodsServlet {
         return byteArrayOutputStream.toByteArray();
     }
 
-    //@des check xdp file
-    // private boolean isXdpFile(javax.servlet.http.Part part) {
-    //     return part.getContentType().equalsIgnoreCase("application/vnd.adobe.xdp+xml");
-    // }
-    
+    // Check if the file is an XDP file
+    private boolean isXdpFile(javax.servlet.http.Part part) {
+        return part.getContentType().equalsIgnoreCase("application/vnd.adobe.xdp+xml");
+    }
 }
