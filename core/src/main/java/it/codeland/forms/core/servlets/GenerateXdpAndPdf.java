600f6df45577af53320244eeaa67ac936c64110a
Abdulkeza <adeoabdul.n@gmail.com>
2024-02-06 13:48:55 +0200

    allow upload of xdp file from insomnia

--- core/src/main/java/it/codeland/forms/core/servlets/GenerateXdpAndPdf.java
+++ core/src/main/java/it/codeland/forms/core/servlets/GenerateXdpAndPdf.java
@@ -17,9 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.adobe.aemfd.docmanager.Document;
-import com.adobe.fd.forms.api.FormsService;
-
-import it.codeland.forms.core.utils.PdfConversionUtils;
+import com.adobe.pdfg.result.CreatePDFResult;
 import com.adobe.pdfg.service.api.GeneratePDFService;
 
 @Component(service = { Servlet.class }, property = { "sling.servlet.methods=POST",
@@ -29,39 +27,38 @@ public class GenerateXdpAndPdf extends SlingAllMethodsServlet {
     private static final long serialVersionUID = 1L;
 
     @Reference
-    FormsService formsService;
-
-    @Reference
     GeneratePDFService generatePDFService;
-
-    @Reference
-    PdfConversionUtils pdfConversionUtils;
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) {
         log.error("++++++++++++++++we are starting now now +++++++++++++++++");
         String file_path = request.getParameter("save_location");
-        String xdpTemplatePath = request.getParameter("xdp_template_path");
 
         InputStream pdf_document_is = null;
+        InputStream xdp_document_is = null;
         InputStream xml_is = null;
         javax.servlet.http.Part pdf_document_part = null;
+        javax.servlet.http.Part xdp_document_part = null;
         javax.servlet.http.Part xml_data_part = null;
         try {
             pdf_document_part = request.getPart("pdf_file");
+            xdp_document_part = request.getPart("xdp_file"); // Get XDP file part
             xml_data_part = request.getPart("xml_data_file");
+
             pdf_document_is = pdf_document_part.getInputStream();
+            xdp_document_is = xdp_document_part.getInputStream(); // Get XDP file input stream
             xml_is = xml_data_part.getInputStream();
 
-            if (isXdpFile(pdf_document_part)) {
+            if (isXdpFile(xdp_document_part)) {
                 log.error("############Checking xdp file has started######");
                 // Convert XDP to dynamic PDF with the provided template path
-                pdf_document_is = pdfConversionUtils.convertXdpToDynamicPdf(pdf_document_is, xdpTemplatePath);
+                CreatePDFResult result = generatePDFService.createPDF2(new Document(xdp_document_is), "xdp", "pdf", null, null, null, null);
+                pdf_document_is = result.getCreatedDocument().getInputStream();
+                log.error("############Checking done ######");
             }
 
-            Document data_merged_document = formsService.importData(new Document(pdf_document_is),
-                    new Document(xml_is));
+            Document data_merged_document = new Document(pdf_document_is); // Adjust according to your logic
 
             data_merged_document.copyToFile(new File(file_path));
 
